c793068951d410530eb689abb11ba50ff5d0cb70
julianRVSS <JulianRH_106@outlook.com>
2022-02-12 22:34:09 -0600

    Practica 3

---
+++ ionic-basic/src/app/alumnos/alumnos.page.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { Alumno } from '../models/alumno.model';
+import { AlumnoService } from '../services/alumno.service';
+
+@Component({
+  selector: 'app-alumnos',
+  templateUrl: './alumnos.page.html',
+  styleUrls: ['./alumnos.page.scss'],
+})
+export class AlumnosPage implements OnInit {
+
+  alumnos: Alumno[];
+  nombre: string;
+  matricula: string;
+  estado: string;
+  idActualizar: number;
+  error: boolean=false;
+
+  constructor(private alumnoService: AlumnoService){
+    this.alumnoService.setAlumnos([
+      {id: 1, nombre: 'Juan Manuel', matricula: '154865'},
+      {id: 2, nombre: 'Angel Alexander', matricula: '184756'},
+      {id: 3, nombre: 'Kevin Jared', matricula: '129462'}]);
+      this.alumnos = this.alumnoService.getAlumnos();
+      this.estado = 'guardar';
+   }
+
+  ngOnInit() {
+  }
+
+  public guardar(){
+    if( (this.nombre == undefined || this.nombre == '' ) || 
+      (this.matricula == undefined || this.matricula == '') ) {
+      this.error = true;
+      return;
+    }  
+    let alumno: Alumno={
+      nombre: this.nombre,
+      matricula: this.matricula
+    };
+    if (this.estado ==='actualizar'){
+      alumno.id = this.idActualizar;
+      this.alumnos = this.alumnoService.actualizar(alumno);
+    }
+    if(this.estado === 'guardar'){
+      this.alumnoService.agregarAlumno(alumno);
+      this.alumnos = this.alumnoService.getAlumnos();
+    }
+    this.cancelar();
+  }
+
+  public cancelar(){
+    this.estado = 'guardar';
+    this.matricula = '';
+    this.nombre = '';
+    this.error = false;
+  }
+
+  public eliminar(id:number){
+    this.alumnoService.borrarAlumno(id);
+    this.alumnos = this.alumnoService.getAlumnos();
+  }
+
+  public editar(alumno:Alumno){
+    this.estado = 'actualizar';
+    this.matricula = alumno.matricula;
+    this.nombre = alumno.nombre;
+    this.idActualizar = alumno.id;
+  }
+
+}
---
+++ ionic-basic/src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'presupuesto',
+    loadChildren: () => import('./presupuesto/presupuesto.module').then( m => m.PresupuestoPageModule)
+  },
+  {
+    path: 'presupuesto2',
+    loadChildren: () => import('./presupuesto2/presupuesto2.module').then( m => m.Presupuesto2PageModule)
+  },
+  {
+    path: 'alumnos',
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
