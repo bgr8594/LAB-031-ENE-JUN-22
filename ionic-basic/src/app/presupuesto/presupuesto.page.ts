25a94e102b76eb537cb4bab078b63aa302fcda89
YairReyna <alexander.reynacvd@uanl.edu.mx>
2022-02-12 22:03:53 -0600

    Practica 02

--- ionic-basic/src/app/presupuesto/presupuesto.page.ts
+++ ionic-basic/src/app/presupuesto/presupuesto.page.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Gasto } from '../models/gasto.model';
+import { GastoService } from '../services/gasto.service';
 
 @Component({
   selector: 'app-presupuesto',
   templateUrl: './presupuesto.page.html'
 })
 export class PresupuestoPage implements OnInit {
+  
   public gastos: string[]=['Directos','Fijos', 'Variables'];
   public selectedValue: any;
   public monto: number;
   public resultados: string;
   public errResultados: string='light';
-  constructor() { }
+  public descripcion: string;
+  public tipoGasto: string='';
+  public gasotsList: Gasto[]=[];
+
+  constructor(private gastosService: GastoService) { }
 
   ngOnInit() {
   }
@@ -26,14 +33,25 @@ export class PresupuestoPage implements OnInit {
   }
   guardar(){
     this.resultados ="";
-    if(this.monto!=null && this.selectedValue!=null){
+    if(this.monto!=null && this.selectedValue!=null && this.descripcion!= null){
       this.errResultados = 'success';
-      this.resultados = 'Gasto seleccionado: '+this.selectedValue+' \nMonto: '+this.monto+'\n';
+      this.resultados = 'Gasto seleccionado: '+this.selectedValue+' \nMonto: '+this.monto+'\n'+'Descricion: '+this.descripcion;
+      let gasto: Gasto = {
+      descripcion: this.descripcion,
+      tipo: this.selectedValue,
+      monto: this.monto
+    }
+      this.gastosService.agregar(gasto);
+      this.gasotsList = this.gastosService.getGastos();
     }
     else{
       this.errResultados = 'danger';
       this.resultados ="No a completado los campos del formulario";
     }
   }
-
+  borrarGasto(idGasto: number){
+    this.gastosService.borrarGasto(idGasto);
+    this.gasotsList = this.gastosService.getGastos();
+  }
+  
 }
