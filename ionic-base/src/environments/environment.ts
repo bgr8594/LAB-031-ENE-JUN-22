f932ce61e07f9b88388e7bad2864e1a3f528c335
DanielTrev <danieltrev09@gmail.com>
2022-03-25 23:08:29 -0600

    practica 07

--- ionic-base/src/environments/environment.ts
+++ ionic-base/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyATptb8xR5jAiWkmMM2PpLWZDQMkIT82Es",
+    authDomain: "back-031-prueba-3414d.firebaseapp.com",
+    projectId: "back-031-prueba-3414d",
+    storageBucket: "back-031-prueba-3414d.appspot.com",
+    messagingSenderId: "590821314397",
+    appId: "1:590821314397:web:dd603e2e5786310795e2b4",
+    measurementId: "G-LBEJKTXRW8"
+  }
 };
 
 /*
