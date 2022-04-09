8a5971b203f6b32327bd3ea91ee44bd48f329728
JuanCDiaz1 <juankid2003gmail.com>
2022-04-09 04:42:22 -0500

    practica 06

--- ionic-basic/src/app/app-routing.module.ts
+++ ionic-basic/src/app/app-routing.module.ts
@@ -35,6 +35,15 @@ const routes: Routes = [
     path: 'detalle-receta',
     loadChildren: () => import('./detalle-receta/detalle-receta.module').then( m => m.DetalleRecetaPageModule)
   },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+
 ];
 
 @NgModule({
