b2be78e8e37f6f986f9a6a3afbd80cc2dc338332
Daniel-GómezUANL <danniel.gomez10@gmail.com>
2022-05-14 22:12:31 -0500

    Practica 4

--- ionic-basic/src/app/compontentes/datos-alumno/datos-alumno.component.ts
+++ ionic-basic/src/app/compontentes/datos-alumno/datos-alumno.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Alumno } from 'src/app/models/alumno.model';
 
 @Component({
   selector: 'app-datos-alumno',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datos-alumno.component.scss'],
 })
 export class DatosAlumnoComponent implements OnInit {
+
+  @Input() alumnosList:Alumno[];
+  @Output() eliminar: any = new EventEmitter<Alumno[]>();
+  @Output() editar:  any = new EventEmitter<number>();
 
   constructor() { }
 
