6a9315bbb0fd28a37c09642c1c738d47e6ab9f62
ValeC46 <ivaleria.cordero@gmail.com>
2022-05-22 21:29:51 -0500

    Practica 11

--- ionic-basic/src/app/models/lugar.model.ts
+++ ionic-basic/src/app/models/lugar.model.ts
@@ -1,16 +1,11 @@
 export class Lugar{
     nombre: string;
     id?: string;
-    
-    ubicacion?:{latitud: string, longitud: string}
+    latitud: number;
+    longitud: number;
 
     constructor(){
         this.nombre = ''
     }
 
-    public setUbicacion(latitud: string, longitud: string){
-        this.ubicacion.latitud = latitud;
-
-        this.ubicacion.longitud = longitud;
-    }
 }
