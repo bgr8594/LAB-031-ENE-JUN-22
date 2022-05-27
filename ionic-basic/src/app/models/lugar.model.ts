8c8cf5f3086bdf34de95a7864247b683a1205f12
ivan58300 <59028802+ivan58300@users.noreply.github.com>
2022-05-27 03:19:28 -0500

    Practica 11

--- ionic-basic/src/app/models/lugar.model.ts
+++ ionic-basic/src/app/models/lugar.model.ts
@@ -1,14 +1,15 @@
 export class Lugar {
     nombre: string;
     id?: string;
-    ubicacion?:{latitud:string, longitud:string}
+    latitud:number; 
+    longitud:number;
 
-    constructor(){
-        this.nombre=''
-    }
 
-    public setUbicacion(latitud: string, Longiutd: string){
-        this.ubicacion.latitud= latitud;
-        this.ubicacion.longitud = Longiutd;
+
+        constructor(){
+
+            this.nombre=''
+
+        }
+
     }
-}
