ae0c8a41900f11bf91e09bd601290128b4b2c58d
MaxM07 <maxi64123@gmail.com>
2022-05-28 21:17:49 -0500

    Practica 10

--- ionic-basic/src/app/services/lugar.service.ts
+++ ionic-basic/src/app/services/lugar.service.ts
@@ -13,6 +13,7 @@ export class LugarService {
       nombre:lugar.nombre,
       ubicacion: {longitud:'', latitud:''}
     };
+    return 
     this.dbFirestore.collection('lugar').add(lugarTemp);
   }
 
@@ -32,6 +33,16 @@ export class LugarService {
     catch(err=>{
       console.log(err);
     });
+   }
+   getLugaresChanges(){
+    return this.dbFirestore.collection('lugar').snapshotChanges();
+  }
 
+  updateLugares(id: any, lugar: any){
+   return this.dbFirestore.collection('lugar').doc(id).update(lugar);
+  }
+
+  deleteLugar(id: any){
+    return this.dbFirestore.collection('lugar').doc(id).delete();
   }
 }
